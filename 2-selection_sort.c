eb436cd40034a18b39fbe119d2a976b242a697a4
Alexpena76 <alexpenasilva@gmail.com>
2025-08-17 18:01:05 -0500

    Q2

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * selection_sort-Sorts an array of integers in ascending order(Selection Sort)
+ * @array: Pointer to the array to be sorted
+ * @size: Number of elements in the array
+ *
+ * Return: Void function
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+
+		if (min_index != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+
+			print_array(array, size);
+		}
+	}
+}

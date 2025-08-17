d3c7f859be8be8b1ed0ba1b7cf359bc4517295fc
Alexpena76 <alexpenasilva@gmail.com>
2025-08-17 17:08:34 -0500

    Changed to swap logic inlined

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *next_node;
+    listint_t *current, *next_node, *prev;
 
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
@@ -22,29 +22,24 @@ void insertion_sort_list(listint_t **list)
 
         while (current->prev != NULL && current->prev->n > current->n)
         {
-            swap_with_prev(list, current);
+            prev = current->prev;
+
+            if (prev->prev != NULL)
+                prev->prev->next = current;
+            else
+                *list = current;
+
+            if (current->next != NULL)
+                current->next->prev = prev;
+
+            prev->next = current->next;
+            current->prev = prev->prev;
+            current->next = prev;
+            prev->prev = current;
+
             print_list(*list);
         }
 
         current = next_node;
     }
-}
-
-void swap_with_prev(listint_t **list, listint_t *node)
-{
-    listint_t *prev = node->prev;
-
-    if (prev->prev != NULL)
-        prev->prev->next = node;
-    else
-        *list = node;
-    
-
-    if (node->next != NULL)
-        node->next->prev = prev;
-
-    prev->next = node->next;
-    node->prev = prev->prev;
-    node->next = prev;
-    prev->prev = node;
 }
